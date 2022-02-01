df488ebb98adf32be6cbec5477707c21a2944909
Rahul Kumar <87421972+fitrahul@users.noreply.github.com>
2022-02-01 11:41:40 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,24 @@
+var reg = /React/;
+// var reg = /React/g;
+// var reg = /React/i;
+var string = "JS React Html css React";
+
+// 1. exec() // value with index, null
+// var res = reg.exec(string);
+// console.log(res);
+// var res = reg.exec(string);
+// console.log(res);
+// var res = reg.exec(string);
+// console.log(res);
+
+// 2.test() // true,false
+// var res = reg.test(string);
+// console.log(res);
+
+// 3. match() // character, null
+// var res = string.match(reg);
+// console.log(res);
+
+// 4. search() //index, null
+// var res = string.search(reg);
+// console.log(res);
