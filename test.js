9b3ecefab4841bd2dc28a3619ca034fed0ddffec
Rahul Kumar <87421972+fitrahul@users.noreply.github.com>
2022-02-09 16:26:11 +0530

    Update test.js

--- test.js
+++ test.js
@@ -4,21 +4,21 @@ var reg = /React/;
 var string = "JS React Html css React";
 
 // 1. exec() // value with index, null
-// var res = reg.exec(string);
-// console.log(res);
-// var res = reg.exec(string);
-// console.log(res);
-// var res = reg.exec(string);
-// console.log(res);
+var res = reg.exec(string);
+console.log(res);
+var res = reg.exec(string);
+console.log(res);
+var res = reg.exec(string);
+console.log(res);
 
 // 2.test() // true,false
-// var res = reg.test(string);
-// console.log(res);
+var res = reg.test(string);
+console.log(res);
 
 // 3. match() // character, null
-// var res = string.match(reg);
-// console.log(res);
+var res = string.match(reg);
+console.log(res);
 
 // 4. search() //index, null
-// var res = string.search(reg);
-// console.log(res);
+var res = string.search(reg);
+console.log(res);
